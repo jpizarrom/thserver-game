aa40a76e3c8fa61c53ff54b293534fef803e912c
Juan Pizarro <jpizarro@doingit.cl>
2011-04-04 02:21:41 -0400

    add username

--- thserver-game/src/main/java/com/jpizarro/th/server/game/view/web/components/user/details/UserDetailsPanel.java
+++ thserver-game/src/main/java/com/jpizarro/th/server/game/view/web/components/user/details/UserDetailsPanel.java
@@ -1,6 +1,11 @@
 package com.jpizarro.th.server.game.view.web.components.user.details;
 
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
+
+import com.jpizarro.th.server.game.view.web.application.WicketApplication;
+import com.jpizarro.th.server.game.view.web.session.WicketSession;
 
 public class UserDetailsPanel extends Panel {
 	/**
@@ -9,7 +14,8 @@ public class UserDetailsPanel extends Panel {
 	private static final long serialVersionUID = -2469138314488437609L;
 	public UserDetailsPanel(String id, String username) {
 		super(id);
-		// TODO Auto-generated constructor stub
+		this.add(new Label("login", username));
+
 	}
 
 
