0b6d22c72b9e9f71aa7dc98c70db3dccbcd9f9e0
Juan Pizarro <jpizarro@doingit.cl>
2011-04-04 02:18:03 -0400

    add UserDetailsPanel

---
+++ thserver-game/src/main/java/com/jpizarro/th/server/game/view/web/components/user/details/UserDetailsPanel.java
@@ -0,0 +1,16 @@
+package com.jpizarro.th.server.game.view.web.components.user.details;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class UserDetailsPanel extends Panel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2469138314488437609L;
+	public UserDetailsPanel(String id, String username) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
