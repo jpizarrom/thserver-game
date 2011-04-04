2a658066b41226e30b06d7ee758864ed3206a80e
Juan Pizarro <jpizarro@doingit.cl>
2011-04-03 23:11:40 -0400

    add UserServiceImpl

---
+++ thserver-game/src/main/java/com/jpizarro/th/server/user/view/rest/client/UserServiceImpl.java
@@ -0,0 +1,106 @@
+package com.jpizarro.th.server.user.view.rest.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jpizarro.th.lib.user.entity.UserTO;
+import com.jpizarro.th.lib.user.entity.response.LoginResultTO;
+import com.jpizarro.th.lib.user.entity.response.UpdatePersonalInfoTO;
+import com.jpizarro.th.lib.user.entity.response.UserRegisterTO;
+import com.jpizarro.th.server.generic.model.persistence.util.exceptions.DuplicateInstanceException;
+import com.jpizarro.th.server.generic.model.persistence.util.exceptions.InstanceNotFoundException;
+//import com.jpizarro.th.server.user.model.entity.User;
+//import com.jpizarro.th.server.user.model.persistence.accessor.UserAccessor;
+import com.jpizarro.th.server.user.model.service.UserService;
+import com.jpizarro.th.server.user.model.service.util.exceptions.IncorrectPasswordException;
+//import com.jpizarro.th.server.user.util.UserUtils;
+import com.thoughtworks.xstream.XStream;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	@Autowired
+	private UserRestClient userRestClient;
+
+//	public UserTO find(long userId) throws InstanceNotFoundException {
+//		User user = userAccessor.find(userId);
+//		System.out.println("IOIO : " + user);
+//		UserTO userTO = new UserTO();
+//		userTO.setUserId( user.getUserId());
+//		userTO.setUsername( user.getUsername());
+//		userTO.setPassword( user.getPassword());
+////		userTO.set = user.getRole();
+////		if (user.getTeam() != null && user.getTeam().getGame()!= null)
+////			userTO.setGameId(user.getTeam().getGame().getGameId());
+//		
+//		userTO.setLatitude( user.getLatitude());
+//		userTO.setLongitude( user.getLongitude());
+//		
+//		return userTO;
+//	}
+
+	@Override
+	public LoginResultTO login(String username, String password)
+			throws InstanceNotFoundException, IncorrectPasswordException {
+		// TODO Auto-generated method stub
+		LoginResultTO user = this.userRestClient.login(username, password);;
+		return user;
+	}
+
+	@Transactional
+	public boolean register(UserRegisterTO usernameInfoTO)
+			throws DuplicateInstanceException {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	@Transactional
+	public boolean changePassword(String username, String oldPassword,
+			String newPassword) throws InstanceNotFoundException,
+			IncorrectPasswordException {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public boolean updatePersonalInfo(String username,
+			UpdatePersonalInfoTO updatePersonalInfoTO)
+			throws InstanceNotFoundException {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	@Transactional
+	public UserTO create(UserTO entity) throws DuplicateInstanceException {
+		throw new NotImplementedException();		
+	}
+
+	@Override
+	public UserTO find(Long id) throws InstanceNotFoundException {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public boolean exists(Long id) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	@Transactional
+	public UserTO update(UserTO entity) throws InstanceNotFoundException {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	@Transactional
+	public void remove(Long id) throws InstanceNotFoundException {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public UserTO findUserByUserName(String username){
+		throw new NotImplementedException();
+	}
+
+}
